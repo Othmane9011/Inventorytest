36f46d813c818a405f5436ac9c89ad640361d2b3
othmane9011 <bengharbi12@gmail.com>
2024-06-30 22:51:23 +0200

    updated

--- my-frontend/src/FileUpload.js
+++ my-frontend/src/FileUpload.js
@@ -15,6 +15,9 @@ const FileUpload = () => {
       return;
     }
 
+    /* Add the aws config here */
+
+
     const s3 = new AWS.S3({
       apiVersion: '2006-03-01',
       params: { Bucket: 'inventaire' },
